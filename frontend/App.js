2bd2a751189ad63e7d794ecd1149b8d6bc2bb091
AnuU21 <43321439+AnuU21@users.noreply.github.com>
2024-02-22 01:03:20 -0500

    Update App.js
    Style of buttons modified, gallery introduced

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,57 @@
-import { Button, View } from 'react-native';
-import {Gallery} from './Gallery'
+import { TouchableOpacity, View, Text, Image, StyleSheet} from 'react-native';
+import Gallery from './Gallery';
 
 export default function App()
 {
-  return 
-  {
-    <View style={{flex: 1, backgroundColor: "white", padding: 60 }}>
-      <Button title="Previous" onPress={() => console.log("Previous movie")}/>
-      <Button title="Like" onPress={() => console.log("Movie liked")}/>
-      <Button title="Next" onPress={() => console.log("Next movie")}/>
-      <Text>
-        <Text style={{color: "gray"}}>Movie Name</Text>Hello World
-      </Text>
-      <div>
-        {
-          Gallery(Image)
-        }
-      </div>
-      <Image>
+  const images = [
+    'https://picsum.photos/200'
+  ];
+  return (
+    <View style={styles.container}>
+      <Gallery images={images}/>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => console.log("Previous pressed")}>
+        <Text style={styles.buttonText}>Previous</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => console.log("Like pressed")}>
+        <Text style={styles.buttonText}>Like</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => console.log("Next pressed")}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Django Unchained</Text>
+      <Image
         source={{uri: "https://www.imdb.com/title/tt1853728/mediaviewer/rm958180352/"}}
         style={{width:300, height:300}}
-      </Image>
+      />
     </View>
-  };
-}
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
 

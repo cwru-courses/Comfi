e495596783f2fe2c32abe000c06dce555d80118e
AnuU21 <43321439+AnuU21@users.noreply.github.com>
2024-02-22 01:02:06 -0500

    Introducing swipe functionality to Gallery.js

--- frontend/Gallery.js
+++ frontend/Gallery.js
@@ -1,9 +1,29 @@
+import {View, Dimensions, Image} from 'react-native';
+import Swiper from 'react-native-swiper';
+
 const Gallery = ({ images }) => {
     return (
-    <div class="gallery">
-    {images.map((image) => (<img key={image.id} src={image.imageUrl} alt={`Img ${image.id}`} />
-    ))}
-    </div>
+    <Swiper style={styles.wrapper} showsButtons={false}>
+        {images.map((image, index) => (
+            <View style={styles.slide} key={index}>
+                <Image style={styles.image} source={{ uri:image }} />
+            </View>
+        ))}
+    </Swiper>
     );
     };
+
+    const styles = {
+        wrapper: {},
+        slide: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'transparent',
+        },
+        image: {
+          width,
+          flex: 1,
+        },
+      };
     export default Gallery;

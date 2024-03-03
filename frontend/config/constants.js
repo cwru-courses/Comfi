5394ae52747b7af6600e3a289909f572930977ba
ulisess1 <usoto1016@gmail.com>
2024-03-03 11:54:32 -0500

    TMDB API key

--- frontend/config/constants.js
+++ frontend/config/constants.js
@@ -2,4 +2,4 @@ export const ENDPOINT_BASE_URL = 'CHANGE_TO_YOUR_IP';
 
 export const TMDB_BASE_POSTER_URL = 'https://image.tmdb.org/t/p/original';
 
-export const TMDB_API_KEY = 'API_KEY';
+export const TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZWNiYTIxZmI5ZWRlNzM4MDVlNWQwYTRmZGI1NDRiOSIsInN1YiI6IjYzZmZiNDA4Njk5ZmI3MDBjNjRhNmMzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JNcGKz-C2-B_6mGxqbN6aFrhMey_6FSeJD9ApWe1N9Q';
